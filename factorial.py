1696f2af5863205c6582a1d0dadd68cc69767b53
Cokops <gerasev-z@mail.ru>
2025-11-22 10:43:25 +0300

    Add GeneratorFibonachi

--- factorial.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-
-num = int(input("Введите число , пожалуйста!\n"))
-
-def factorial(n):
-    return reduce(lambda x, y: x * y, range(1, n + 1))
-
-print(factorial(num))
--- prostieChisla.py
+++
@@ -1,7 +0,0 @@
-def prosto(limit):
-    for num in range(2, limit + 1):
-        if all(num % i != 0 for i in range(2, int(num ** 0.5) + 1)):
-            yield num
-
-for prostie in prosto(30):
-    print(prostie)
